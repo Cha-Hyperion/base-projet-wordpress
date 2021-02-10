8c93880757b54d7bb95280c9214532148ab9bfcb
Cha-Hyperion <63230036+Cha-Hyperion@users.noreply.github.com>
2021-02-10 17:43:55 +0100

    install

---
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
+
+require get_theme_file_path( 'inc/assets.php' );
+require get_theme_file_path( 'inc/cookies.php' );
